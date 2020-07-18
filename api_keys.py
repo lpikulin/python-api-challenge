dd61313066588c0c8b499e51c79d172221c03e5e
lpikulin <linda.89oldhwy28@gmail.com>
2020-07-17 21:14:43 -0400

    no api

--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "0ef79d26458c94e986b967c13962d9ae"
+weather_api_key = ""
 
 # Google API Key
-g_key = "AIzaSyAiowfHxy_RRRi5XeUYqTJ-vEoizHXOnAY"
+g_key = ""
